79e26b6df02b55f6a8fa465beaed1920d76b8303
sofiabergstrom <sofia.bergstrom@scilifelab.se>
2019-06-18 18:50:58 +0200

    Making first documentation for bead_count.R

--- R/bead_count.R
+++ R/bead_count.R
@@ -1,15 +1,24 @@
 #' Bead count
 #'
+#' Bead count
+#'
 #' Investigate if you have enough beads in each well of each beadID
 #'
 #'
-#'
+#' @section Warnings:
+#' This is a warning
 #' @param bead_count_file A dataframe with bead count. Each row is one well, each column is a bead.
-#' @return Plot
+#' @return Plot with bead count per sample and per bead
 #' @examples
 #' bead_count(df)
 #' @export
 
+bead_count <- function(x) {
+  for(i in 1:x){
+    cat(rep( letters[i], times = i),"\n")
+  }
+  # Check if second object can be called by our function
+  cat(second_object)
+}
 
 
-print("Hej på dig")
