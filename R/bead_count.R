68f6905b450a416cb0d880f3857ac6c0cedc12c0
sofiabergstrom <sofia.bergstrom@scilifelab.se>
2019-06-19 14:24:19 +0200

    Continue working with ggplot

--- R/bead_count.R
+++ R/bead_count.R
@@ -19,7 +19,9 @@
 
 bead_count_base <- function(bead_count_df, bead_count_limit = 32, path_for_output = "./", date_in_file_name = T, ...) {
 
-  file_name_and_path <- paste0(path_for_output, paste0(Sys.Date() ," Bead count with base.pdf"))
+
+  file_name_and_path <- ifelse(date_in_file_name == T, paste0(path_for_output, paste0(Sys.Date() ," Bead count with base.pdf")),
+                               paste0(path_for_output, paste0(" Bead count with base.pdf")))
 
   #par(oma= c(0.5, 0.5, 0.5, 0.5), mar=c(8.1, 8.1, 4.1, 4.1))
   pdf(file = file_name_and_path, width= 16, height= 8)
@@ -55,13 +57,13 @@ bead_count_base <- function(bead_count_df, bead_count_limit = 32, path_for_outpu
           las=2
   )
 
-  # beeswarm(t(bead_count_df), add = TRUE, cex = 0.5, pch = 16, col = "grey62", corral = "gutter",  ...)
-  #
-  # # To get the boxes above the beeswarm
-  # boxplot(t(bead_count_df), main = "", cex.main = 2.5, names = F, xaxt = "n",
-  #         pch = "", cex = 0.5,
-  #         log = "y",las=2, add = T
-  # )
+  beeswarm(data.frame(t(bead_count_df)), add = TRUE, cex = 0.5, pch = 16, col = "grey62", corral = "gutter",  ...)
+
+  # To get the boxes above the beeswarm
+  boxplot(t(bead_count_df), main = "", cex.main = 2.5, names = F, xaxt = "n",
+          pch = "", cex = 0.5,
+          log = "y",las=2, add = T
+  )
 
   abline(h= bead_count_limit, col= "skyblue2")
   mtext("Number of beads", side = 2, line = 2.8, las = 3, cex = 1.2, font = 2)
@@ -72,4 +74,94 @@ dev.off()
 
 
 
+
+
+
+
+
+
+#' --------------------------------------------------------------------------------------------------
+#' Bead count with ggplot
+#' --------------------------------------------------------------------------------------------------
+#' Investigate if you have enough beads in each well of each beadID
+#'
+#'
+#'
+#'
+#' @section Warnings:
+#' This is a warning
+#' @param bead_count_df A dataframe with bead count. Each row is a well, each column is a bead.
+#' @param bead_count_limit How many beads do you need? Can be vector if you want several lines.
+#' @param data_in_file_name Yes or No.
+#' @return Plot with bead count per sample and per bead
+#' @examples
+#' bead_count(df)
+#' @export
+
+
+library(reshape2)
+library(ggplot2)
+library(tidyr)
+
+
+bead_count_ggplot <- function(bead_count_df, bead_count_limit = 32, path_for_output = "./", date_in_file_name = T, ...) {
+
+
+  file_name_and_path <- ifelse(date_in_file_name == T, paste0(path_for_output, paste0(Sys.Date() ," Bead count with base.pdf")),
+                               paste0(path_for_output, paste0(" Bead count with base.pdf")))
+
+  sofia <- gather(df_bead_count, key = "Bead", value = "Count", colnames(df_bead_count), na.rm = FALSE, convert = FALSE)
+
+
+  #par(oma= c(0.5, 0.5, 0.5, 0.5), mar=c(8.1, 8.1, 4.1, 4.1))
+  pdf(file = file_name_and_path, width= 16, height= 8)
+
+
+  ggplot(sofia, aes(x=Bead, y=Count)) +
+    geom_boxplot()
+
+  # --------------------------------------------------------------------------------------------------
+  # Per antibody
+  # --------------------------------------------------------------------------------------------------
+  # boxplot(bead_count_df, main = "Bead count", cex.main = 2, names = F, xaxt = "n",
+  #         pch = "", cex = 0.5,
+  #         #log = "y",
+  #         las=2
+  # )
+  #
+  # beeswarm(bead_count_df, add = TRUE, cex = 0.5, pch = 16, col = "grey62", corral = "gutter",  ...)
+  #
+  # # To get the boxes above the beeswarm
+  # boxplot(bead_count_df, main = "", cex.main = 2.5, names = F, xaxt = "n",
+  #         pch = "", cex = 0.5,
+  #         las=2, add = T
+  # )
+  #
+  # abline(h= bead_count_limit, col= "skyblue2")
+  # mtext("Number of beads", side = 2, line = 2.8, las = 3, cex = 1.2, font = 2)
+  # mtext(paste0("BeadID (n = ", length(colnames(bead_count_df)), ")"), side = 1, line = 2, las = 1, cex = 1.2, font = 2)
+  #
+  # # --------------------------------------------------------------------------------------------------
+  # # Per sample
+  # # --------------------------------------------------------------------------------------------------
+  # boxplot(t(bead_count_df), main = "Bead count", cex.main = 2, names = F, xaxt = "n",
+  #         pch = "", cex = 0.5,
+  #         #log = "y",
+  #         las=2
+  # )
+  #
+  # beeswarm(data.frame(t(bead_count_df)), add = TRUE, cex = 0.5, pch = 16, col = "grey62", corral = "gutter",  ...)
+  #
+  # # To get the boxes above the beeswarm
+  # boxplot(t(bead_count_df), main = "", cex.main = 2.5, names = F, xaxt = "n",
+  #         pch = "", cex = 0.5,
+  #         log = "y",las=2, add = T
+  # )
+  #
+  # abline(h= bead_count_limit, col= "skyblue2")
+  # mtext("Number of beads", side = 2, line = 2.8, las = 3, cex = 1.2, font = 2)
+  # mtext(paste0("Samples (n = ", length(rownames(bead_count_df)), ")"), side = 1, line = 2, las = 1, cex = 1.2, font = 2)
+
+  dev.off()
+}
 
