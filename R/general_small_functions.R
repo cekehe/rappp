548460213eb3cccd1eef6d8d1d56808879d4a300
C Hellstrom <amirell87@gmail.com>
2019-07-09 10:22:19 +0200

    Fix typos in cv documentation.

--- R/general_small_functions.R
+++ R/general_small_functions.R
@@ -6,10 +6,10 @@
 #' If set to TRUE, then missing values are removed before computation proceeds.
 #'
 #' @param x Vector or matrix with values.
-#' @param format If the output should be as "percent" (default), or "decimal".
+#' @param format If the output should be as "percent" (default) or "decimal".
 #' @param digits Integer indicating the number of decimal places (see \link[stats]{round})
 #' @param ... Further arguments passed do \link[stats]{mean} and \link[stats]{sd}
-#' @return List of MADs, with same structure as input list.
+#' @return The CV of the input values.
 #' @export
 
 cv <- function(x, format="percent", digits=2, ...) {
